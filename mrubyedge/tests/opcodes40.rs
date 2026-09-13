23d2f311f8ba8c0a5e58db0aebdb5a74fa8a7c60
Uchio Kondo <udzura@udzura.jp>
2026-09-13 15:12:00 +0900

    fmt

--- mrubyedge/tests/opcodes40.rs
+++ mrubyedge/tests/opcodes40.rs
@@ -2,19 +2,19 @@ extern crate mrubyedge;
 
 mod helpers;
 
-#[path = "opcodes40/idx0.rs"]
-mod idx0;
-#[path = "opcodes40/send0.rs"]
-mod send0;
 #[path = "opcodes40/blkcall.rs"]
 mod blkcall;
-#[path = "opcodes40/ret.rs"]
-mod ret;
-#[path = "opcodes40/ilv.rs"]
-mod ilv;
 #[path = "opcodes40/definition.rs"]
 mod definition;
+#[path = "opcodes40/idx0.rs"]
+mod idx0;
+#[path = "opcodes40/ilv.rs"]
+mod ilv;
 #[path = "opcodes40/matcherr.rs"]
 mod matcherr;
+#[path = "opcodes40/ret.rs"]
+mod ret;
+#[path = "opcodes40/send0.rs"]
+mod send0;
 #[path = "opcodes40/whole.rs"]
 mod whole;
